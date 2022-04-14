c201069888fe59aa0911ed5717a9c0dc2bbfa25d
TAHİR MAT <matahir33@gmail.com>
2022-04-14 19:34:21 +0300

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from  translate_input_text import translate_inputtext2eng
 translator = Translator()
 
 st.title('Text To Image Application')
-INPUT_TEXT = st.text_input("Type what you want to draw: ")
-INPUT_TEXT.click()
-result = translate_inputtext2eng(translator, INPUT_TEXT)
-st.write(result.text)
+# INPUT_TEXT = st.text_input("Type what you want to draw: ")
+# INPUT_TEXT.click()
+# result = translate_inputtext2eng(translator, INPUT_TEXT)
+# st.write(result.text)
