260e6442732aa6acd341d689fc9358866b46d3f8
TAHİR MAT <matahir33@gmail.com>
2022-04-14 20:03:22 +0300

    .

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 import streamlit as st 
 from googletrans import Translator
 from  translate_input_text import translate_inputtext2eng
@@ -7,4 +8,7 @@ translator = Translator()
 st.title('Text To Image Application')
 INPUT_TEXT = st.text_input("Type what you want to draw: ")
 result = translator.translate(INPUT_TEXT)
-st.text_area(result.text)
+
+for i in result.split():
+    result = re.match(r"\bS\w+", i)
+    st.text_area(result.text)
