80b974c29faac53c5278bb4c900d7f54ad2aee2e
TAHİR MAT <matahir33@gmail.com>
2022-04-14 19:10:17 +0300

    https://github.com/Tahiromat/text2image_withAI.git

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+from googletrans import Translator
+from  translate_input_text import translate_inputtext2eng
+
+translator = Translator()
+
+st.title('Text To Image Application')
+INPUT_TEXT = st.text_input("Type what you want to draw: ")
+result = translate_inputtext2eng(translator, INPUT_TEXT)
+st.write(result.text)
---
+++ translate_input_text.py
@@ -0,0 +1,8 @@
+
+
+
+
+
+def translate_inputtext2eng(translator, INPUT_TEXT):
+    result = translator.translate(INPUT_TEXT)
+    return result
